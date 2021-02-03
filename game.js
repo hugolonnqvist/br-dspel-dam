753f7fcbcbb066e465a59b95bf5765ea7a0549e7
Hugo <hugolonnqvistosterberg@gmail.com>
2021-02-03 11:05:25 +0100

    first commit

---
+++ game.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Pawn  {
+    constructor(img, xCord, yCord) {
+        this.img = img;
+        this.xCord = xCord;
+        this.yCord = yCord;
+    }
+}
+
+let img = document.createElement("img");
+
+let squares = document.getElementsByClassName("square");
