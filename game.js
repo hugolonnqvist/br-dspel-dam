93048c66c055c8fa325ea20c265782ea55dc363d
Hugo <hugolonnqvistosterberg@gmail.com>
2021-02-05 13:28:19 +0100

    Trying to create a board with black and white squares

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 "use strict";
 
-class Pawn  {
+class Pawn {
     constructor(img, xCord, yCord) {
         this.img = img;
         this.xCord = xCord;
@@ -8,6 +8,33 @@ class Pawn  {
     }
 }
 
-let img = document.createElement("img");
-
 let squares = document.getElementsByClassName("square");
+
+function generateBoard() {
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            if ((i + j) % 2 === 0){
+                squares[].innerHTML = "x";
+            }
+                
+        }
+    }
+}
+
+generateBoard();
+
+
+/*
+for (let i = 0; i < squares.length; i++) {
+    if (i % 2 === 0) {
+        if ((i >= 10 && i < 20) || ( i>= 30 && i < 40) || (i >= 70 && i < 80) || (i >= 90 && i < 100)) {
+            squares[i].innerHTML = "o";
+        }
+    }
+    else if (i % 2 !== 0) {
+        if ((i < 10) || (i >= 20 && i < 30) || (i >= 80 && i < 90) || i >= 60 && i < 70) {
+            squares[i].innerHTML = "o";
+
+        }
+    }
+}*/
