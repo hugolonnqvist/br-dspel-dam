b8289146d9f7386e013aad5564119a283ab63225
Hugo <hugolonnqvistosterberg@gmail.com>
2021-02-12 13:06:49 +0100

    Completing the board and creating a add generate pices func

--- game.js
+++ game.js
@@ -1,28 +1,56 @@
 "use strict";
 
-class Pawn {
-    constructor(img, xCord, yCord) {
-        this.img = img;
-        this.xCord = xCord;
-        this.yCord = yCord;
-    }
-}
 
-let squares = document.getElementsByClassName("square");
+function createBoard() {
+    let board = document.querySelector(".board");
+    let counter = 0;
+    let flag = false;
 
-function generateBoard() {
-    for (let i = 1; i <= 10; i++) {
-        for (let j = 1; j <= 10; j++) {
-            if ((i + j) % 2 === 0){
-                squares[].innerHTML = "x";
-            }
-                
+    for (let i = 0; i < 100; i++) {
+        let square = [];
+        square[i] = document.createElement("div");
+
+        if (flag) {
+            square[i].classList.add("square");
+            square[i].classList.add("black");
+            counter++;
+            flag = !flag;
+
+        } else {
+            square[i].classList.add("square");
+            square[i].classList.add("white");
+            counter++;
+            flag = !flag;
+        }
+        board.append(square[i]);
+        if (counter === 10) {
+            flag = !flag;
+            counter = 0;
         }
     }
 }
 
-generateBoard();
 
+let block = document.getElementsByClassName("square");
+
+function generatePieces() {
+    let blackSquare = document.getElementsByClassName("square black");
+
+    for (let i = 0; i < 50; i++) {
+        if (i >= 0 && i< 20) {
+            blackSquare[i].style.color = "white";
+            blackSquare[i].innerHTML = "O";
+            console.log(blackSquare[i]);
+        }
+        else if (i >= 30 && i <= 50) {
+            blackSquare[i].style.color = "white";
+            blackSquare[i].innerHTML = "O"
+        }
+    }
+}
+
+createBoard();
+generatePieces();
 
 /*
 for (let i = 0; i < squares.length; i++) {
