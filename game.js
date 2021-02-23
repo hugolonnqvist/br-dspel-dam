e0b7139df3a454edea24810d53aa344ee26418ac
Hugo <hugolonnqvistosterberg@gmail.com>
2021-02-23 12:54:33 +0100

    Changing how the board and pices generate

--- game.js
+++ game.js
@@ -1,13 +1,25 @@
 "use strict";
 
-function coordinateToIndex(x, y) {
-    return y * boardWidth + x;
-}
+//function coordinateToIndex(x, y) {
+//    let boardWidth = 8;
+//   return y * boardWidth + x - 1;
+//}
+//
+
+let board = [
+    [0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0]
+];
 
 
-
-function createBoard() {
-    let board = document.querySelector(".board");
+function generateBoard() {
+    let boardDiv = document.querySelector(".board");
     let counter = 0;
     let flag = false;
 
@@ -27,7 +39,7 @@ function createBoard() {
             counter++;
             flag = !flag;
         }
-        board.append(square[i]);
+        boardDiv.append(square[i]);
         if (counter === 8) {
             flag = !flag;
             counter = 0;
@@ -36,43 +48,21 @@ function createBoard() {
 }
 
 
-function generatePieces() {
-    let blackSquare = document.getElementsByClassName("square black");
+function genPieces() {
+    let square = document.getElementsByClassName("square");
 
-    for (let i = 0; i < 12; i++) {
-        blackSquare[i].innerHTML = `<img class="pawn" src="./imges/readpawn.png">`;
-        blackSquare[i + 20].innerHTML = `<img class="pawn" src="./imges/readpawn.png">`;
-    }
-
-    let piece = document.getElementsByClassName("pawn");
-   
-    for (let i = 0; i < piece.length; i++) {
-        piece[i].addEventListener("click",() => {
-        //Highlighta rutorna som pjäsen kan flytta till
-        })
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (board[i][j] == 2) {
+                square[i * 8 + j].innerHTML = `<img class="pawn" src="./imges/readpawn.png">`;
+            }
+            else if (board[i][j] == 1) {
+                square[i * 8 + j].innerHTML = `<img class="pawn" src="./imges/whitepawn.png">`;
+            }
+        }
     }
 }
 
-function movePieces() {
-    
-}
 
-createBoard();
-generatePieces();
-movePieces();
-
-
-/*
-for (let i = 0; i < squares.length; i++) {
-    if (i % 2 === 0) {
-        if ((i >= 10 && i < 20) || ( i>= 30 && i < 40) || (i >= 70 && i < 80) || (i >= 90 && i < 100)) {
-            squares[i].innerHTML = "o";
-        }
-    }
-    else if (i % 2 !== 0) {
-        if ((i < 10) || (i >= 20 && i < 30) || (i >= 80 && i < 90) || i >= 60 && i < 70) {
-            squares[i].innerHTML = "o";
-
-        }
-    }
-}*/
+generateBoard();
+genPieces();
