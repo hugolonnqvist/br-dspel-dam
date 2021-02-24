3086c4eca9e495c6bc333f64eab6a96ae94790db
Hugo <hugolonnqvistosterberg@gmail.com>
2021-02-24 10:09:41 +0100

    changing the structure of the code

--- game.js
+++ game.js
@@ -1,68 +1,139 @@
 "use strict";
 
-//function coordinateToIndex(x, y) {
-//    let boardWidth = 8;
-//   return y * boardWidth + x - 1;
-//}
-//
+function generateBoard(board) {
+  function createSquare(flag) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.classList.add(flag ? "black" : "white");
+    return square;
+  }
 
-let board = [
-    [0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0],
-    [0, 2, 0, 2, 0, 2, 0, 2],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0]
+  function populateBoard(elements) {
+    const boardDiv = document.querySelector(".board");
+    elements.forEach((row) => row.forEach((cell) => boardDiv.append(cell)));
+  }
+
+  const elements = board.map((row, rowIndex) => {
+    let flag = rowIndex % 2;
+
+    return row.map(() => {
+      const square = createSquare();
+      flag = !flag;
+      return square;
+    });
+  });
+
+  populateBoard(elements);
+}
+
+const board = [
+  [0, 2, 0, 2, 0, 2, 0, 2],
+  [2, 0, 2, 0, 2, 0, 2, 0],
+  [0, 2, 0, 2, 0, 2, 0, 2],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0],
 ];
 
+function generatePieces() {
+  let square = document.getElementsByClassName("square");
 
-function generateBoard() {
-    let boardDiv = document.querySelector(".board");
-    let counter = 0;
-    let flag = false;
-
-    for (let i = 0; i < 64; i++) {
-        let square = [];
-        square[i] = document.createElement("div");
-
-        if (flag) {
-            square[i].classList.add("square");
-            square[i].classList.add("black");
-            counter++;
-            flag = !flag;
-
-        } else {
-            square[i].classList.add("square");
-            square[i].classList.add("white");
-            counter++;
-            flag = !flag;
-        }
-        boardDiv.append(square[i]);
-        if (counter === 8) {
-            flag = !flag;
-            counter = 0;
-        }
+  board.forEach((row, rowIndex) =>
+    row.forEach((cell, cellIndex) => {
+      let color = null;
+      if (cell === 1) {
+        color = "white";
+      } else if (cell === 2) {
+        color = "red";
+      }
+      if (color) {
+        square[
+          rowIndex * 8 + cellIndex
+        ].innerHTML = `<img class="pawn" src="./images/${color}pawn.png">`;
+      }
+    })
+  );
+  /*
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      let color = null;
+      if (board[i][j] == 2) {
+        color = "red";
+      } else if (board[i][j] == 1) {
+        color = "white";
+      }
+      if (color) {
+        square[
+          coordToIndex(i, j)
+        ].innerHTML = `<img class="pawn" src="./images/${color}pawn.png">`;
+      }
     }
+  }*/
 }
 
+function coordToIndex(y, x) {
+  let boardWidth = 8;
+  return y * boardWidth + x;
+}
+/*
+function movePiece(param) {
 
-function genPieces() {
+
     let square = document.getElementsByClassName("square");
+    let turn = false;
+    let once = {
+        once: true
+    };
 
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            if (board[i][j] == 2) {
-                square[i * 8 + j].innerHTML = `<img class="pawn" src="./imges/readpawn.png">`;
-            }
-            else if (board[i][j] == 1) {
-                square[i * 8 + j].innerHTML = `<img class="pawn" src="./imges/whitepawn.png">`;
+    if (!turn) {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (board[i][j] == 1) {
+                    square[coordToIndex(i, j)].addEventListener("click", function () {
+
+                        
+                        square[coordToIndex(i, j) - 7].innerHTML += `<img class="dot" src="./imges/readpawn.png">`;
+                        square[coordToIndex(i, j) - 9].innerHTML += `<img class="dot" src="./imges/readpawn.png">`;
+
+                        let possibleMove = document.getElementsByClassName("dot");
+                        
+                        possibleMove.addEventListener("click", () => {
+                            square[]
+                        })
+
+                        square[coordToIndex(i, j) - 7].addEventListener("click", () => {
+                            square[coordToIndex(i, j) - 7].innerHTML = square[i * 8 + j].innerHTML;
+                            square[coordToIndex(i, j)].innerHTML = null;
+                            board[i][j] = 0;
+                            board[i - 1][j + 1] = 1;
+                            turn = !turn;
+                            console.log(turn);
+                            update();
+                        }, once)
+
+                        square[coordToIndex(i, j) - 9].addEventListener("click", () => {
+                            square[coordToIndex(i, j) - 9].innerHTML = square[i * 8 + j].innerHTML;
+                            square[coordToIndex(i, j)].innerHTML = null;
+                            board[i][j] = 0;
+                            board[i - 1][j - 1] = 1;
+                            turn = !turn; 
+                            console.log(turn);
+                            update();
+                        }, once)
+
+
+                    }, once);
+                }
             }
         }
+
+    } else if (turn) {
+        console.log("reds turn");
     }
-}
+}*/
 
-
-generateBoard();
-genPieces();
+generateBoard(board);
+generatePieces();
+// movePiece();
